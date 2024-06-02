ccac2c0ff3baa3a1bf7af196c0814d27b3a9c17f
reid3333 <120766169+reid3333@users.noreply.github.com>
2024-06-03 00:39:41 +0900

    pushd/popdは-Cオプションに移行 & cloneへ進む分岐が抜けていた

--- SdxlWebUi/setup/GitCloneOrPull.sh
+++ SdxlWebUi/setup/GitCloneOrPull.sh
@@ -16,35 +16,34 @@ GIT_CLONE_OR_PULL_DIR=$(basename "$GIT_CLONE_OR_PULL_URL" .git)
 
 # リポジトリの存在確認と URL の確認
 if [ -d "$GIT_CLONE_OR_PULL_DIR" ]; then
-    pushd "$GIT_CLONE_OR_PULL_DIR" > /dev/null
-    REMOTE_ORIGIN_URL=$(git config --get remote.origin.url)
+    REMOTE_ORIGIN_URL=$(git -C ${GIT_CLONE_OR_PULL_DIR} config --get remote.origin.url)
 
     if [ "$GIT_CLONE_OR_PULL_URL" == "$REMOTE_ORIGIN_URL" ]; then
         CHECKOUT_NAME="$2"
         if [ ! -z "$CHECKOUT_NAME" ]; then
-            git checkout "$CHECKOUT_NAME"
+            git -C ${GIT_CLONE_OR_PULL_DIR} checkout -f "$CHECKOUT_NAME"
         fi
 
-        git pull
+        git -C ${GIT_CLONE_OR_PULL_DIR} pull
         if [ $? -ne 0 ]; then
             echo "Failed to pull changes"
-            popd > /dev/null
             exit 1
         fi
+        exit 0
     else
-        popd > /dev/null
-        echo "Removing directory due to URL mismatch"
-        rm -rf "$GIT_CLONE_OR_PULL_DIR"
+        # TODO: 不要ディレクトリを削除するスクリプトを別途作る
+        echo "Renaming directory due to URL mismatch"
+        DST_DIR="__obsolute__${GIT_CLONE_OR_PULL_DIR}"
+        mv -f ${GIT_CLONE_OR_PULL_DIR} ${DST_DIR}
         if [ $? -ne 0 ]; then
-            echo "Failed to remove directory"
+            echo "Failed to rename directory"
             exit 1
         fi
     fi
-    popd > /dev/null
-else
-    git clone "$GIT_CLONE_OR_PULL_URL"
-    if [ $? -ne 0 ]; then
-        echo "Failed to clone repository"
-        exit 1
-    fi
+fi
+
+git clone "$GIT_CLONE_OR_PULL_URL"
+if [ $? -ne 0 ]; then
+    echo "Failed to clone repository"
+    exit 1
 fi
